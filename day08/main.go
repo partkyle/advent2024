72bf56bf62f263ae10db04cc70315dba4361e3f9
partkyle <partkyle@gmail.com>
2024-12-16 12:35:04 -0800

    love it when it uses gcf

--- day08/main.go
+++ day08/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"iter"
+	"math"
 	"slices"
 
 	"advent2024/util"
@@ -25,6 +26,48 @@ func (v Vector) Sub(o Vector) Vector {
 func (v Vector) Within(lo Vector, hi Vector) bool {
 	return lo.x <= v.x && v.x < hi.x &&
 		lo.y <= v.y && v.y < hi.y
+}
+
+func (v Vector) Simplify() Vector {
+	f := gcf(int(math.Abs(float64(v.x))), int(math.Abs(float64(v.y))))
+	return Vector{v.x / f, v.y / f}
+}
+
+func gcf(a, b int) int {
+	af := factors(a)
+	bf := factors(b)
+
+	c := make(map[int]struct{})
+	for _, a := range af {
+		c[a] = struct{}{}
+	}
+
+	var v []int
+	for _, b := range bf {
+		if _, ok := c[b]; ok {
+			v = append(v, b)
+		}
+	}
+
+	return v[0]
+}
+
+func factors(a int) []int {
+	var f []int
+	for i := 1; i <= int(math.Ceil(math.Sqrt(float64(a)))); i++ {
+		if a%i == 0 {
+			if !slices.Contains(f, i) {
+				f = append(f, i)
+			}
+			if !slices.Contains(f, a/i) {
+				f = append(f, a/i)
+			}
+		}
+	}
+
+	slices.Sort(f)
+	slices.Reverse(f)
+	return f
 }
 
 func UniquePairs[E any](l []E) iter.Seq[[2]E] {
@@ -57,20 +100,7 @@ func main() {
 
 	fmt.Printf("%+v\n", bounds)
 
-	antisignals := make(map[Vector]struct{})
-	for _, v := range groupedAntennas {
-		for pair := range UniquePairs(v) {
-			pointA := pair[0].Sub(pair[1].Sub(pair[0]))
-			pointB := pair[1].Sub(pair[0].Sub(pair[1]))
-
-			if pointA.Within(Vector{}, bounds) {
-				antisignals[pointA] = struct{}{}
-			}
-			if pointB.Within(Vector{}, bounds) {
-				antisignals[pointB] = struct{}{}
-			}
-		}
-	}
+	antisignals := pt2(groupedAntennas, bounds)
 
 	for y := 0; y < bounds.y; y++ {
 		for x := 0; x < bounds.x; x++ {
@@ -86,4 +116,41 @@ func main() {
 	}
 
 	fmt.Println(len(antisignals))
+}
+
+func pt1(groupedAntennas map[rune][]Vector, bounds Vector) map[Vector]struct{} {
+	antisignals := make(map[Vector]struct{})
+	for _, v := range groupedAntennas {
+		for pair := range UniquePairs(v) {
+			pointA := pair[0].Sub(pair[1].Sub(pair[0]))
+			pointB := pair[1].Sub(pair[0].Sub(pair[1]))
+
+			if pointA.Within(Vector{}, bounds) {
+				antisignals[pointA] = struct{}{}
+			}
+			if pointB.Within(Vector{}, bounds) {
+				antisignals[pointB] = struct{}{}
+			}
+		}
+	}
+	return antisignals
+}
+
+func pt2(groupedAntennas map[rune][]Vector, bounds Vector) map[Vector]struct{} {
+	antisignals := make(map[Vector]struct{})
+	for _, v := range groupedAntennas {
+		for pair := range UniquePairs(v) {
+			dirA := pair[1].Sub(pair[0]).Simplify()
+			dirB := pair[0].Sub(pair[1]).Simplify()
+
+			for pointA := pair[0]; pointA.Within(Vector{}, bounds); pointA = pointA.Sub(dirA) {
+				antisignals[pointA] = struct{}{}
+			}
+
+			for pointB := pair[0]; pointB.Within(Vector{}, bounds); pointB = pointB.Sub(dirB) {
+				antisignals[pointB] = struct{}{}
+			}
+		}
+	}
+	return antisignals
 }
--- day08/main_test.go
+++ day08/main_test.go
@@ -113,3 +113,145 @@ func TestVector_Within(t *testing.T) {
 		})
 	}
 }
+
+func TestVector_Simplify(t *testing.T) {
+	type fields struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Vector
+	}{
+		{
+			name: "simplify to 1/2",
+			fields: fields{
+				x: 2,
+				y: 4,
+			},
+			want: Vector{
+				x: 1,
+				y: 2,
+			},
+		},
+		{
+			name: "simplify to 2/1",
+			fields: fields{
+				x: 16,
+				y: 8,
+			},
+			want: Vector{
+				x: 2,
+				y: 1,
+			},
+		},
+		{
+			name: "simplify to 16/1",
+			fields: fields{
+				x: 16,
+				y: 1,
+			},
+			want: Vector{
+				x: 16,
+				y: 1,
+			},
+		},
+		{
+			name: "simplify to -4/1",
+			fields: fields{
+				x: -16,
+				y: 4,
+			},
+			want: Vector{
+				x: -4,
+				y: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := Vector{
+				x: tt.fields.x,
+				y: tt.fields.y,
+			}
+			if got := v.Simplify(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Simplify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_factors(t *testing.T) {
+	type args struct {
+		a int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "4",
+			args: args{a: 4},
+			want: []int{4, 2, 1},
+		},
+		{
+			name: "15",
+			args: args{a: 15},
+			want: []int{15, 5, 3, 1},
+		},
+		{
+			name: "31",
+			args: args{a: 15},
+			want: []int{31, 1},
+		},
+		{
+			name: "44",
+			args: args{a: 15},
+			want: []int{44, 22, 11, 4, 2, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := factors(tt.args.a); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("factors() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_gcf(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1,11",
+			args: args{a: 1, b: 11},
+			want: 1,
+		},
+		{
+			name: "5,15",
+			args: args{a: 5, b: 15},
+			want: 5,
+		},
+		{
+			name: "200, 112",
+			args: args{a: 200, b: 112},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := gcf(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("gcf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
