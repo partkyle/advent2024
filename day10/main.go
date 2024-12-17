96d3279b127a04b0ced5daa3659cf59c7d282b9d
partkyle <partkyle@gmail.com>
2024-12-17 00:48:03 -0800

    part 2

--- day10/main.go
+++ day10/main.go
@@ -29,12 +29,26 @@ func main() {
 		}
 	}
 
+	pt2(hikingMap)
+}
+
+func pt1(hikingMap map[util.Vector]int) {
 	var total int
 	for k, v := range hikingMap {
 		if v == 0 {
 			trailheads := make(map[util.Vector]bool)
 			traverse(hikingMap, trailheads, k, v)
 			total += len(trailheads)
+		}
+	}
+	fmt.Println(total)
+}
+
+func pt2(hikingMap map[util.Vector]int) {
+	var total int
+	for k, v := range hikingMap {
+		if v == 0 {
+			total += rating(hikingMap, k, v)
 		}
 	}
 	fmt.Println(total)
@@ -58,4 +72,28 @@ func traverse(hikingMap map[util.Vector]int, trailheads map[util.Vector]bool, k 
 			traverse(hikingMap, trailheads, pos, v+1)
 		}
 	}
+}
+
+func rating(hikingMap map[util.Vector]int, k util.Vector, v int) int {
+	if v == 9 {
+		return 1
+	}
+
+	directions := []util.Vector{
+		{X: 0, Y: -1},
+		{X: 0, Y: 1},
+		{X: -1, Y: 0},
+		{X: 1, Y: 0},
+	}
+
+	var total int
+	for _, direction := range directions {
+		pos := k.Add(direction)
+		val := hikingMap[pos]
+		if val == v+1 {
+			total += rating(hikingMap, pos, v+1)
+		}
+	}
+
+	return total
 }
