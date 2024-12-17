430dde7f136607520135af73f90b3b40d94e9840
partkyle <partkyle@gmail.com>
2024-12-17 10:31:33 -0800

    dynamic programming

--- day11/main.go
+++ day11/main.go
@@ -77,7 +77,7 @@ func transform(line string) []int {
 }
 
 func main() {
-	pt1()
+	pt2()
 }
 
 func pt1() {
@@ -94,4 +94,83 @@ func pt1() {
 	}
 
 	fmt.Println(list.Count())
+}
+
+type internalStruct struct {
+	val   int
+	times int
+}
+
+type memoized struct {
+	data map[internalStruct]int
+}
+
+func newMemoized() *memoized {
+	return &memoized{
+		data: make(map[internalStruct]int),
+	}
+}
+
+func (m memoized) HasValue(val int, times int) bool {
+	_, ok := m.data[internalStruct{val: val, times: times}]
+	return ok
+}
+
+func (m memoized) GetValue(val int, times int) int {
+	v, _ := m.data[internalStruct{val: val, times: times}]
+	return v
+}
+
+func (m memoized) SetValue(val int, times int, total int) {
+	m.data[internalStruct{val: val, times: times}] = total
+}
+
+func blinkValue(m *memoized, val int, times int) int {
+	if m.HasValue(val, times) {
+		return m.GetValue(val, times)
+	}
+
+	if times == 0 {
+		return 1
+	}
+
+	var stones []int
+	if val == 0 {
+		stones = append(stones, 1)
+	} else {
+		digits := ToDigits(val)
+		if len(digits)%2 == 0 {
+			stones = append(stones, ToInt(digits[0:len(digits)/2]))
+			stones = append(stones, ToInt(digits[len(digits)/2:]))
+		} else { // odd
+			stones = append(stones, val*2024)
+		}
+	}
+
+	var total int
+	for _, stone := range stones {
+		total += blinkValue(m, stone, times-1)
+	}
+
+	m.SetValue(val, times, total)
+
+	return total
+}
+
+func pt2() {
+	var stones []int
+	for line := range util.DataProcess(DAY, transform) {
+		for _, val := range line {
+			stones = append(stones, val)
+		}
+	}
+
+	fmt.Println(stones)
+
+	memo := newMemoized()
+	var total int
+	for _, stone := range stones {
+		total += blinkValue(memo, stone, 75)
+	}
+	fmt.Println(total)
 }
